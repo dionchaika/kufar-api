c33f80b479d24e01a395aded6f9a745e0dc500db
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-05-15 16:09:21 +0300

    Update Flat.php

--- src/Adverts/Flat.php
+++ src/Adverts/Flat.php
@@ -135,7 +135,6 @@ class Flat implements AdvertInterface
      * @throws \InvalidArgumentException
      */
     public function __construct(
-
         string $subject,
         int $rooms,
         string $body,
