ae3f2cebb9e9d78c300c87dfe10ff233bc168cee
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-05-15 14:40:02 +0300

    Update Kufar.php

--- src/Kufar.php
+++ src/Kufar.php
@@ -256,8 +256,8 @@ class Kufar
 
             'redirects' => true,
 
-            'debug'              => $debug,
-            'debug_file'         => $debugFile
+            'debug'      => $debug,
+            'debug_file' => $debugFile
 
         ];
 
